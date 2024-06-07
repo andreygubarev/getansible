52c8058b13258dd62a42a43da972a5d8ab960ba3
Andrey Gubarev <andrey@andreygubarev.com>
2024-06-07 16:04:48 +0400

    updated environment variables lc_all and lang to c in entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -43,8 +43,8 @@ fi
 mkdir -p "$ANSIBLE_HOME/collections"
 export ANSIBLE_COLLECTIONS_PATH
 
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
+export LC_ALL=C
+export LANG=C
 
 ### assert | ansible galaxy compatibility ####################################
 assert_ansible_galaxy() {
