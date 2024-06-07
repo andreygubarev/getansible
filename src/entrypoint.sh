3010efb7b0d8f05425520d89f381ed05561c3491
Andrey Gubarev <andrey@andreygubarev.com>
2024-06-07 13:08:23 +0400

    refactor ansible playbook execution, add pip requirements, ansible roles, collections, and galaxy support

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -24,6 +24,8 @@ fi
 
 ### environment | ansible #####################################################
 export ANSIBLE_HOME="$WORKDIR/.ansible"
+mkdir -p "$ANSIBLE_HOME"
+
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
@@ -47,31 +49,22 @@ assert_ansible_galaxy() {
 
 ### function | playbook #######################################################
 playbook() {
-    playbook_dir=$1
-    playbook_subdir=$2
+    workspace=$1
 
-    workdir="$playbook_dir"
-    if [ -n "$playbook_subdir" ]; then
-        workdir="$playbook_dir/$playbook_subdir"
-    fi
-
-    pushd "$workdir" > /dev/null || exit 1
-
-    # if there is only one file in the playbook_dir and it is a directory, cd into it
-    if [ "$(find . -maxdepth 1 -type f | wc -l)" -eq 0 ] && [ "$(find . -maxdepth 1 -type d | wc -l)" -eq 2 ]; then
+    pushd "$workspace" > /dev/null || exit 1
+    # change directory if there is only one sub-directory in the workspace
+    if { [ "$(find . -maxdepth 1 -type f | wc -l)" -eq 0 ]; } && \
+       { [ "$(find . -maxdepth 1 -type d | wc -l)" -eq 2 ]; }
+    then
         subdir=$(find . -maxdepth 1 -type d -not -name .)
         popd > /dev/null || exit 1
-        pushd "$workdir/$subdir" > /dev/null || exit 1
+        pushd "$workspace/$subdir" > /dev/null || exit 1
     fi
 
     ANSIBLE_PLAYBOOK_DIR=$(pwd)
     export ANSIBLE_PLAYBOOK_DIR
 
-    if [ ! -f playbook.yml ]; then
-        echo "No playbook.yml found"
-        exit 5
-    fi
-
+    # workspace: dotenv
     if [ -f .env ]; then
         while IFS= read -r var || [[ -n "$var" ]]; do
             if [[ ! "$var" == "" ]] && [[ ! "$var" == \#* ]]; then
@@ -84,8 +77,43 @@ playbook() {
         done < .env
     fi
 
+    # workspace: pip requirements
+    if [ -f requirements.txt ]; then
+        "$WORKDIR"/bin/python3 -m pip install --no-cache-dir -r requirements.txt
+    fi
+
+    # workspace: ansible playbook
+    workspace_playbook=""
+    if [ -f playbook.yml ]; then
+        workspace_playbook="playbook.yml"
+    elif [ -f playbook.yaml ]; then
+        workspace_playbook="playbook.yaml"
+    else
+        echo "ERROR: playbook.yml not found"
+        exit 1
+    fi
+
+    # workspace: ansible roles
+    if [ -z "${ANSIBLE_ROLES_PATH:-}" ]; then
+        export ANSIBLE_ROLES_PATH="$ANSIBLE_PLAYBOOK_DIR/roles"
+    fi
+    mkdir -p "$ANSIBLE_ROLES_PATH"
+
+    # workspace: ansible collections
+    if [ -z "${ANSIBLE_COLLECTIONS_PATH:-}" ]; then
+        export ANSIBLE_COLLECTIONS_PATH="$ANSIBLE_PLAYBOOK_DIR/collections"
+    fi
+    mkdir -p "$ANSIBLE_COLLECTIONS_PATH"
+
+    # workspace: ansible galaxy
+    if [ -f requirements.yml ]; then
+        "$WORKDIR"/bin/ansible-galaxy install -r requirements.yml
+        echo $?
+    fi
+
+    # workspace: ansible inventory
     if [ -z "${ANSIBLE_INVENTORY:-}" ]; then
-        tmphosts=$(mktemp)
+        tmphosts="$WORKDIR/.ansible/hosts.tmp"
 
         if [ -p /dev/stdin ]; then
             cat - > "$tmphosts"
@@ -93,22 +121,22 @@ playbook() {
 
         if [ -s "$tmphosts" ]; then
             if [ "$(head -n 1 "$tmphosts")" == "---" ]; then
-                cp "$tmphosts" "$(pwd)/hosts.yml"
-                ANSIBLE_INVENTORY="$(pwd)/hosts.yml"
+                cp "$tmphosts" "$ANSIBLE_PLAYBOOK_DIR/hosts.yml"
+                ANSIBLE_INVENTORY="$ANSIBLE_PLAYBOOK_DIR/hosts.yml"
             else
-                cp "$tmphosts" "$(pwd)/hosts"
-                ANSIBLE_INVENTORY="$(pwd)/hosts"
+                cp "$tmphosts" "$ANSIBLE_PLAYBOOK_DIR/hosts"
+                ANSIBLE_INVENTORY="$ANSIBLE_PLAYBOOK_DIR/hosts"
             fi
         elif [ -f hosts ]; then
-            ANSIBLE_INVENTORY="$(pwd)/hosts"
+            ANSIBLE_INVENTORY="$ANSIBLE_PLAYBOOK_DIR/hosts"
         elif [ -f hosts.yml ]; then
-            ANSIBLE_INVENTORY="$(pwd)/hosts.yml"
+            ANSIBLE_INVENTORY="$ANSIBLE_PLAYBOOK_DIR/hosts.yml"
         fi
 
         export ANSIBLE_INVENTORY
-        rm -rf "$tmphosts"
     fi
 
+    # workspace: ansible inventory (localhost)
     if [ ! -f host_vars/localhost.yml ]; then
         mkdir -p host_vars
         touch host_vars/localhost.yml
@@ -117,30 +145,11 @@ playbook() {
         echo "ansible_python_interpreter: $WORKDIR/bin/python3" >> host_vars/localhost.yml
     fi
 
-    if [ -f requirements.txt ]; then
-        "$WORKDIR"/bin/pip3 install --no-cache-dir -r requirements.txt
-    fi
-
-    if [ -z "${ANSIBLE_ROLES_PATH:-}" ]; then
-        export ANSIBLE_ROLES_PATH="$ANSIBLE_PLAYBOOK_DIR/roles"
-    fi
-    mkdir -p "$ANSIBLE_ROLES_PATH"
-
-    if [ -z "${ANSIBLE_COLLECTIONS_PATH:-}" ]; then
-        export ANSIBLE_COLLECTIONS_PATH="$ANSIBLE_PLAYBOOK_DIR/collections"
-    fi
-    mkdir -p "$ANSIBLE_COLLECTIONS_PATH"
-
-    if [ -f requirements.yml ]; then
-        "$WORKDIR"/bin/ansible-galaxy install -r requirements.yml
-        echo $?
-    fi
-
-    "$WORKDIR"/bin/ansible-playbook playbook.yml
+    # workspace: execute
+    "$WORKDIR"/bin/ansible-playbook "$workspace_playbook"
     rc=$?
 
     popd > /dev/null || exit 1
-    rm -rf "$playbook_dir"
     return $rc
 }
 
@@ -176,7 +185,6 @@ main() {
 
         # use case 1.2.2: file tarball relative path (relative path to *.tar.gz playbook, e.g "myplaybook.tar.gz")
         elif echo "$location" | grep -q '\.tar\.gz$'; then
-            location="$location"
             location_type="tarball"
 
         else
@@ -231,7 +239,7 @@ main() {
             assert_ansible_galaxy
 
             if [ -n "$playbook_version" ]; then
-                location_version="==$url_version"
+                location_version="==$playbook_version"
             else
                 location_version=""
             fi
